277ea030a4c1070af76272e531e9bccfb7a06243
jsungg <jsung@pointable.ai>
2023-09-07 13:25:45 -0700

    add script to see coverage for both typescript and typescript-openai libraries

--- typescript-openai/vite.config.ts
+++ typescript-openai/vite.config.ts
@@ -1,10 +1,16 @@
 // vite.config.ts
+/// <reference types="vitest" />
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
